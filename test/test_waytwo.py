042569c5160f307f9055938be19c2c63a33e7526
Gowtham Sai <gowtham95india@gmail.com>
2018-04-28 02:44:12 +0530

    when initalising sets up browser for further communicating with api

--- test/test_waytwo.py
+++ test/test_waytwo.py
@@ -1,4 +1,7 @@
 import unittest
+
+import mock
+
 import waytwo
 
 
@@ -22,3 +25,30 @@ class TestWayTwoInitalisation(unittest.TestCase):
         self.assertRaises(TypeError, waytwo.WayTwo, self.username)
         self.assertRaises(TypeError, waytwo.WayTwo, self.password)
         self.assertTrue(isinstance(self.waytwo, waytwo.WayTwo))
+
+
+class TetLogin(unittest.TestCase):
+    "WayTwo Login"
+
+    def setUp(self):
+        self.username = '1234567890'
+        self.password = 'password'
+        self.mocked_browser = mock.patch(
+            'waytwo.mechanize.Browser', autospec=True).start()
+        self.addCleanup(mock.patch.stopall)
+
+    def test_browser_should_mask_as_user(self):
+        mocked_browser = self.mocked_browser.return_value
+        waytwo.WayTwo(self.username, self.password)
+        mocked_browser.assert_called_once
+        mocked_browser.set_cookiejar.assert_called_once
+        mocked_browser.set_handle_equiv.assert_called_once
+        mocked_browser.set_handle_redirect.assert_called_once
+        mocked_browser.set_handle_referer.assert_called_once
+        mocked_browser.set_handle_robots.assert_called_once
+        mocked_browser.set_handle_refresh.assert_called_once
+        self.assertEqual(mocked_browser.addheaders, [(
+            'User-agent',
+            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)'
+            + ' Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
+            )])
--- waytwo/__init__.py
+++ waytwo/__init__.py
@@ -1,6 +1,23 @@
 __version__ = '0.0.1'
 __all__ = ['WayTwo']
 
+import mechanize
+import cookielib
+
+
 class WayTwo(object):
     def __init__(self, username, password):
-      pass
+        self._browser = mechanize.Browser()
+        cookie_jar = cookielib.LWPCookieJar()
+        self._browser.set_cookiejar(cookie_jar)
+        self._browser.set_handle_equiv(True)
+        self._browser.set_handle_redirect(True)
+        self._browser.set_handle_referer(True)
+        self._browser.set_handle_robots(False)
+        self._browser.set_handle_refresh(
+            mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        self._browser.addheaders = [(
+            'User-agent',
+            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)'
+            + ' Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
+            )]
